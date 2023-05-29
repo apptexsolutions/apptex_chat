2384b84797439bc4e4a62586319dba58077588c2
sayedidrees <sayedidrees47@gmail.com>
2023-05-29 11:59:11 +0500

    minor ui chnages

--- example/lib/chat_screen.dart
+++ example/lib/chat_screen.dart
@@ -31,102 +31,14 @@ class _CustomChatScreenState extends State<CustomChatScreen> {
       conversationModel: widget.model,
       showMicButton: true,
       appBarBuilder: ((currentUser, otherUser) => AppBar()),
-      // typingWidget: Padding(
-      //   padding: EdgeInsets.symmetric(horizontal: 20),
-      //   //margin: EdgeInsets.only(bottom: 32, left: 32),
-      //   child: Row(
-      //     children: [
-      //       Expanded(
-      //         child: Container(
-      //           padding: EdgeInsets.symmetric(horizontal: 20),
-      //           decoration: BoxDecoration(
-      //             color: Colors.grey.withOpacity(0.2),
-      //             borderRadius: BorderRadius.circular(40),
-      //           ),
-      //           child: Row(
-      //             children: [
-      //               Icon(Icons.sentiment_satisfied_alt_outlined),
-      //               SizedBox(width: 10),
-      //               Expanded(
-      //                 child: TextField(
-      //                   controller: _controller,
-      //                   decoration: InputDecoration(
-      //                     border: InputBorder.none,
-      //                     hintText: "Type a message",
-      //                   ),
-      //                   onChanged: (value) {
-      //                     if (value.isNotEmpty)
-      //                       setState(() {
-      //                         showSendButton = true;
-      //                       });
-      //                     else
-      //                       setState(
-      //                         () {
-      //                           showSendButton = false;
-      //                         },
-      //                       );
-      //                   },
-      //                 ),
-      //               ),
-      //               Icon(Icons.attach_file),
-      //               SizedBox(width: 10),
-      //               Icon(Icons.camera_alt),
-      //             ],
-      //           ),
-      //         ),
-      //       ),
-      //       SizedBox(width: 10),
-      //       Visibility(
-      //         visible: showSendButton,
-      //         child: InkWell(
-      //           onTap: () {
-      //             apptexChat.sendTextMessage(_controller.text);
-      //             _controller.clear();
-      //             setState(() {
-      //               showSendButton = false;
-      //             });
-      //           },
-      //           child: Container(
-      //             padding: EdgeInsets.all(15),
-      //             decoration: BoxDecoration(
-      //               color: Colors.blue,
-      //               shape: BoxShape.circle,
-      //             ),
-      //             child: Icon(
-      //               Icons.send,
-      //               color: Colors.white,
-      //             ),
-      //           ),
-      //         ),
-      //         replacement: Container(
-      //           padding: EdgeInsets.all(15),
-      //           decoration: BoxDecoration(
-      //             color: Colors.blue,
-      //             shape: BoxShape.circle,
-      //           ),
-      //           child: Icon(
-      //             Icons.mic,
-      //             color: Colors.white,
-      //           ),
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
       bubbleBuilder: (model, currentUser, otherUser, isMine) {
         final code = model.code;
         if (code == 'TXT') {
-          return MessageBubble(
-            model: model,
-          );
+          return MessageBubble(model: model);
         } else if (code == 'IMG') {
-          return ImageBubble(
-            model: model,
-          );
+          return ImageBubble(model: model);
         } else if (code == 'MP3') {
-          return AudioBubble(
-            model: model,
-          );
+          return AudioBubble(model: model);
         } else {
           return Container();
         }
